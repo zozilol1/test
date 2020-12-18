f69cc30b80a7fe718034421826498bed01e3b5b8
朱亚军 <2561733232@qq.com>
2020-12-18 20:26:49 +0800

    存入for循环方法

--- src/main/java/cn/tedu/HelloWorld.java
+++ src/main/java/cn/tedu/HelloWorld.java
@@ -3,5 +3,11 @@ package cn.tedu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        int num=0;
+        for (int i=0;i<5;i++){
+            num+=i;
+        }
+        System.out.println(num);
+
     }
 }
